75b467e73064d4715ba0d80c863e09ad195cce99
nthanhthan <102190139@sv1.dut.udn.vn>
2023-04-09 18:40:50 +0700

    inital

---
+++ src/test/java/com/example/ecovelo/EcoveloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ecovelo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcoveloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
